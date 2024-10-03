2016e49b9621a029e0b5c380e72d41524952bad9
123HiEUaa <155602762+123HiEUaa@users.noreply.github.com>
2024-10-03 20:59:37 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap chieu rong hinh chu nhat");
+        float width = float.Parse(Console.ReadLine());
+        Console.WriteLine("nhap chieu cao hinh chu nhat");
+        float height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("chu vi hinh chu nhat la {0}", (width+height)*2);
+        Console.WriteLine($"Dien thich hinh chu nhat la {width*height}");
+    }
+}
